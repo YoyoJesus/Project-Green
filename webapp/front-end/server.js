51657eca2e68d4986653d9b72ebfc2bcdabe9317
YoyoJesus <51387449+YoyoJesus@users.noreply.github.com>
2024-04-21 08:29:25 -0400

    fixed/created web functionality

--- webapp/front-end/server.js
+++ webapp/front-end/server.js
@@ -31,6 +31,28 @@ app.post('/updateVariable', (req, res) => {
   });
 });
 
+app.post('/decrementValue', (req, res) => {
+  fs.readFile('variable.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading variable:', err);
+      res.status(500).send('Error reading variable');
+    } else {
+      const currentValue = parseInt(data);
+      const amount = parseInt(req.body);
+      const newValue = currentValue - amount;
+
+      fs.writeFile('variable.txt', newValue.toString(), (err) => {
+        if (err) {
+          console.error('Error updating variable:', err);
+          res.status(500).send('Error updating variable');
+        } else {
+          res.send(newValue.toString());
+        }
+      });
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
